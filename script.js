3390fcff1cad67f28c08005b07e2cf751114724e
tolentinovallerie <tolentinovallerie@gmail.com>
2024-01-04 09:02:59 -0700

    blahblah

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".burger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
